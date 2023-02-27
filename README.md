c59e4f68cbb71d4dca19aa069bfd1ed0563bf468
Patrick Skinner <pas8314@gmail.com>
2023-02-27 00:32:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Indexing from scratch for fun
+Having benefitted from `CREATE INDEX` over many years' worth of webapp buildling, I realized I was kind of conceptualizing it as free performance pixie dust and hadn't implemented an index from scratch before. So when I randomly saw this [comment](https://news.ycombinator.com/item?id=30594233)° on the Orange Site I made a note to actually try it on a weekend as a miniproject. That weekend finally arrived! 🎉
+
+So basically [index-fun.py](./index-fun.py) is the result of me attempting to build baby's first index in Python, plus a binary search slightly modified to accomodate my ugly "index" 🤡 file.
+The point was to take direct inspiration from how dead tree book indexes work - the text file is literally just a text file with sorted lines of
+`$word,$index` e.g. `foo, 38`
+
+Results:
+```
+tablescan found wiz @ 9971
+tablescan runtime 0.0005388259887695312
+binarySearch found wiz @ 9971
+index runtime 0.0000209808
+```
+
+### TODO:
+- [ ] Try something like this again but with btrees
+
+<details>
+<summary>° copy of the comment for convenience:</summary>
+
+```
+Take a large unordered text file and then create an ordered index in a separate file of word ==> line
+write a brief binary search algo to search the index.
+Compare searching the words with a "table scan" on the first file using grep, vs the binary search on the index.
+You will find the table scan is O(n) and your binary search is roughly O(log n)
+In 60 minutes you'll understand more about indexing than reading stack overflow.
+```
+
+</details>
---
+++ index-fun.py
@@ -0,0 +1,54 @@
+import time
+
+def tablescan(searchterm):
+  with open('unordered.txt', 'r') as file:
+    words = file.read().splitlines()
+    starttime = time.time()
+    for i in range(len(words)):
+      if words[i] == searchterm:
+        print(f"tablescan found {searchterm} @ {i}")
+  endtime = time.time()
+  print('tablescan runtime', endtime - starttime)
+
+def makeIndex():
+  with open('unordered.txt', 'r') as file:
+    with open('unordered.idx', 'w') as indexFile:
+      words = file.read().splitlines()
+      for i in range(len(words)):
+        # make baby's first 'index' file 🤡
+        words[i] = words[i] + "," + str(i) + "\n"
+      words.sort()
+      indexFile.writelines(words)
+
+def binarySearch(A, i):
+  left = 0
+  right = len(A) - 1
+  while left <= right:
+    middle = left + (right - left) // 2
+    # we want only the value, so exclude the comma + rest
+    value = A[middle].split(',')[0]
+    if value == i:
+      return middle
+    elif value < i:
+      left = middle + 1
+    else:
+      right = middle - 1
+  return -1
+
+def search(searchterm):
+  with open('unordered.idx', 'r') as indexFile:
+    indexLines = indexFile.read().splitlines()
+    starttime = time.time()
+    resultIndex = binarySearch(indexLines, searchterm)
+    endtime = time.time()
+    if resultIndex != -1:
+      index = indexLines[resultIndex].split(',')[1]
+      with open('unordered.txt', 'r') as file:
+        words = file.read().splitlines()
+        print(f"binarySearch found {searchterm} @ {index}")
+        print(f"this should read {searchterm}:", words[int(index)])
+        print('index runtime', '{:.10f}'.format(endtime - starttime))
+
+# makeIndex()
+tablescan("wiz")
+search("wiz")
